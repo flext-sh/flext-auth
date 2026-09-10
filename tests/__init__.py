41e0d0aaec07c22a9f9eaaf5d5ef77abd15aa9ed
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-09 22:52:19 -0300

    fixes

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,9 +9,8 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_tests import FlextTestsConstants, d, e, h, r, td, tf, tk, tm, tv, x
-
     from flext_auth import c as auth_c
+    from flext_tests import FlextTestsConstants, d, e, h, r, td, tf, tk, tm, tv, x
 
     from . import fixtures, unit
     from .base import TestsFlextAuthServiceBase, TestsFlextAuthServiceBase as s
