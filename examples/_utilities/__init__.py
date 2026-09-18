c031f87c6364c52d27866b8bdcf91f5cd9d09e04
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-18 11:15:45 -0300

    fixes

--- examples/_utilities/__init__.py
+++ examples/_utilities/__init__.py
@@ -9,16 +9,13 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_auth import c, d, e, h, m, p, r, s, t, x
-
-    from .example_utilities import u
+    from flext_auth import c, d, e, h, m, p, r, s, t, u, x
 __all__: tuple[str, ...] = ("c", "d", "e", "h", "m", "p", "r", "s", "t", "u", "x")
 
 _LAZY_IMPORTS = MappingProxyType(
     build_lazy_import_map(
         MappingProxyType({
-            ".example_utilities": ("u",),
-            "flext_auth": ("c", "d", "e", "h", "m", "p", "r", "s", "t", "x"),
+            "flext_auth": ("c", "d", "e", "h", "m", "p", "r", "s", "t", "u", "x")
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
