66a337165f75345f9bc619e0ed410c67e8e97636
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-23 14:19:00 -0300

    fixes

--- examples/__init__.py
+++ examples/__init__.py
@@ -9,22 +9,9 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_api import api
-    from flext_cli import cli
-    from flext_web import web
+    from flext_api import api, cli, core, d, e, h, lazy_attribute, r, services, web, x
 
     from flext_auth import auth, c, config, m, main, p, s, settings, t, u
-    from flext_core import (
-        core,
-        d,
-        e,
-        h,
-        lazy,
-        lazy_attribute,
-        normalize_lazy_imports,
-        r,
-        x,
-    )
 
     from .basic_usage_flows import FlextAuthBasicUsageFlows
     from .basic_usage_workflow import FlextAuthBasicUsageWorkflow
@@ -42,14 +29,13 @@ __all__: tuple[str, ...] = (
     "d",
     "e",
     "h",
-    "lazy",
     "lazy_attribute",
     "m",
     "main",
-    "normalize_lazy_imports",
     "p",
     "r",
     "s",
+    "services",
     "settings",
     "t",
     "u",
@@ -62,7 +48,19 @@ _LAZY_IMPORTS = MappingProxyType(
         MappingProxyType({
             ".basic_usage_flows": ("FlextAuthBasicUsageFlows",),
             ".basic_usage_workflow": ("FlextAuthBasicUsageWorkflow",),
-            "flext_api": ("api",),
+            "flext_api": (
+                "api",
+                "cli",
+                "core",
+                "d",
+                "e",
+                "h",
+                "lazy_attribute",
+                "r",
+                "services",
+                "web",
+                "x",
+            ),
             "flext_auth": (
                 "auth",
                 "c",
@@ -75,19 +73,6 @@ _LAZY_IMPORTS = MappingProxyType(
                 "t",
                 "u",
             ),
-            "flext_cli": ("cli",),
-            "flext_core": (
-                "core",
-                "d",
-                "e",
-                "h",
-                "lazy",
-                "lazy_attribute",
-                "normalize_lazy_imports",
-                "r",
-                "x",
-            ),
-            "flext_web": ("web",),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
