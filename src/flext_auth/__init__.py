425ee93d0235a5f919752ed0d2157b9d8d8133c5
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-22 23:43:28 -0300

    chore(gen): land generator fixed-point output (pydantic-settings>=2.14.0,<2.15 + allow_redefinition facets)

--- src/flext_auth/__init__.py
+++ src/flext_auth/__init__.py
@@ -20,7 +20,11 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_api import api, cli, core, d, e, h, lazy_attribute, r, web, x
+    from flext_api import api
+    from flext_cli import cli
+    from flext_web import web
+
+    from flext_core import core, d, e, h, lazy_attribute, r, x
 
     from . import providers, services
     from ._config import FlextAuthConfig, config
@@ -155,18 +159,10 @@ _LAZY_IMPORTS = MappingProxyType(
             ".services.token_service": ("FlextAuthTokenService",),
             ".typings": ("FlextAuthTypes", "t"),
             ".utilities": ("FlextAuthUtilities", "u"),
-            "flext_api": (
-                "api",
-                "cli",
-                "core",
-                "d",
-                "e",
-                "h",
-                "lazy_attribute",
-                "r",
-                "web",
-                "x",
-            ),
+            "flext_api": ("api",),
+            "flext_cli": ("cli",),
+            "flext_core": ("core", "d", "e", "h", "lazy_attribute", "r", "x"),
+            "flext_web": ("web",),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
